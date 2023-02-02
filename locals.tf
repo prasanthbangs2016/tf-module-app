d737659d7343e2d102713a63a8281698a56746a1
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 01:34:49 +0530

    added tf file

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+  ssh_username = element(split("/", data.aws_ssm_parameter.ssh_credentials.value ), 0)
+  ssh_password = element(split("/", data.aws_ssm_parameter.ssh_credentials.value ), 1)
+}
+
+locals {
+  SPOT_INSTANCE_ID =aws_spot_instance_request.SPOT.*.spot_instance_id
+  ONDEMAND_INSTANE_ID = aws_instance.ondemand.*.id
+  ALL_INSTANCE_ID = concat(local.SPOT_INSTANCE_ID, local.ONDEMAND_INSTANE_ID)
+  SPOT_PRIVATE_IP = aws_spot_instance_request.SPOT.private_ip
+  ONDEMAND_PRIVATE_IP = aws_instance.ondemand.private_ip
+  ALL_PRIVATE_IP = concat(local.SPOT_PRIVATE_IP,local.ONDEMAND_PRIVATE_IP)
+
+}
--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ondemand" {
   instance_type = var.instances["ONDEMAND"].instance_type
   ami = data.aws_ami.ami.image_id
   subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
+  vpc_security_group_ids = [aws_security_group.main.id]
 
 }
 resource "aws_spot_instance_request" "SPOT" {
@@ -10,21 +11,56 @@ resource "aws_spot_instance_request" "SPOT" {
   instance_type = var.instances["SPOT"].instance_type
   ami = data.aws_ami.ami.image_id
   subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
-  #wait_for_fulfillment = true
-  tags = {
-    Name = "cart-${var.ENV}"
-
+  wait_for_fulfillment = true
   }
-}
-locals {
-  SPOT_INSTANCE_ID =aws_spot_instance_request.SPOT.*.spot_instance_id
-  ONDEMAND_INSTANE_ID = aws_instance.ondemand.*.id
-  ALL_INSTANCE_ID = concat(local.SPOT_INSTANCE_ID, local.ONDEMAND_INSTANE_ID)
-}
-resource "aws_ec2_tag" "name" {
+
+source "aws_ec2_tag" "name" {
   count = length(local.ALL_INSTANCE_ID)
   resource_id = element(local.ALL_INSTANCE_ID, count.index)
   key = "Name"
   value = "${var.COMPONENT}-${var.ENV}"
 
+}
+
+resource "null_resource" "ansible_apply" {
+  count = length(local.ALL_PRIVATE_IP)
+  provisioner "remote-exec" {
+    connection {
+      host = element(local.ALL_PRIVATE_IP, count.index)
+      user = local.ssh_username
+      password = local.ssh_password
+    }
+    inline = [
+      "ansible-pull -i localhost, -U https://github.com/prasanthbangs2016/roboshop-mutable-ansible--v2 roboshop.yml -e HOSTS=localhost -e APP_COMPONENT_ROLE=${var.COMPONENT} -e ENV=${var.ENV}  &>/tmp/cart.log"
+    ]
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = "roboshop-${var.ENV}-${var.COMPONENT}"
+  description = "roboshop-${var.ENV}--${var.COMPONENT}"
+    vpc_id      = data.terraform_remote_state.infra.outputs.vpc_id
+  #to instance
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.infra.outputs.vpc_cidr,data.terraform_remote_state.infra.outputs.WORKSTATION_IP]
+
+  }
+  #outbound from instance
+  egress {
+    description      = "egress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+
+  tags = {
+    Name = "Roboshop-${var.env}-rabbitmq"
+  }
 }
