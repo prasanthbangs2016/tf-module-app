0970483905219d624d36876c98a395fd827c510a
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 00:57:10 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@ resource "aws_instance" "ondemand" {
   count = var.instances["ONDEMAND"].instance_count
   instance_type = var.instances["ONDEMAND"].instance_type
   ami = data.aws_ami.ami.image_id
-  wa
 
 }
 resource "aws_instance" "SPOT" {
   count = var.instances["SPOT"].instance_count
   instance_type = var.instances["SPOT"].instance_type
   ami = data.aws_ami.ami.image_id
-  
+
 }
