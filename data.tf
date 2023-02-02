0e5fa0a0aacc251935306c36b0f7212553dc18bc
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 00:50:45 +0530

    added tf file

---
+++ data.tf
@@ -0,0 +1,25 @@
+data "aws_ssm_parameter" "ssh_credentials" {
+  name = "ssh_credentials"
+
+}
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "centos-devops-practice-ansible"
+  owners           = ["self"]
+
+  #  filter {
+  #    name   = "name"
+  #    values = ["myami-*"]
+  #  }
+  #
+  #  filter {
+  #    name   = "root-device-type"
+  #    values = ["ebs"]
+  #  }
+  #
+  #  filter {
+  #    name   = "virtualization-type"
+  #    values = ["hvm"]
+  #  }
+}
---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "ondemand" {
+  count = var.instances["ONDEMAND"].instance_count
+  instance_type = var.instances["ONDEMAND"].instance_type
+  ami = data.aws_ami.ami.image_id
+
+
+}
+resource "aws_instance" "SPOT" {
+  count = var.instances["SPOT"].instance_count
+  instance_type = var.instances["SPOT"].instance_type
+  ami = data.aws_ami.ami.image_id
+  wait_for_fulfillment = true
+
+
+}
