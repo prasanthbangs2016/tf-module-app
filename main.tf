29ffe0fbd1fb0269812fdec45ef40c8539ac4193
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 01:08:10 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "SPOT" {
   instance_type = var.instances["SPOT"].instance_type
   ami = data.aws_ami.ami.image_id
   subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
-  wait_for_fulfillment = true
+  #wait_for_fulfillment = true
   tags = {
     Name = "cart-${var.ENV}"
 
