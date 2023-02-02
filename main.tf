fc9a89979e3b7455acccdf365b9f49e8a90630d9
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 00:59:08 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -8,5 +8,9 @@ resource "aws_instance" "SPOT" {
   count = var.instances["SPOT"].instance_count
   instance_type = var.instances["SPOT"].instance_type
   ami = data.aws_ami.ami.image_id
+  wait_for_fulfillment = true
+  tags = {
+    Name = "cart-${var.ENV}"
 
+  }
 }
