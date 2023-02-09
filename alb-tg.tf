b79d84bf44a53cb3c100eeb65d3b0d77dd6aaa5a
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-10 00:22:39 +0530

    added tf file

--- alb-tg.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_lb_target_group_attachment" "test" {
-  #as i want target group arn so export info into outputs from alb module and that referenced in infra module
-  #and, as statefile will have info so will reference from it
-  count = length(local.ALL_INSTANCE_ID)
-  target_group_arn = data.terraform_remote_state.infra.outputs.public_tg_arn
-  target_id        = local.ALL_INSTANCE_ID
-  port             = 80
-}
