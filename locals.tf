02ff423d3085e1f48e2cfd6478a29364727dc1bd
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-11 01:41:51 +0530

    added tf file

--- locals.tf
+++ locals.tf
@@ -5,11 +5,12 @@ locals {
 
 locals {
   SPOT_INSTANCE_ID =aws_spot_instance_request.SPOT.*.spot_instance_id
-  ONDEMAND_INSTANE_ID = aws_instance.ondemand.*.id
+  //ONDEMAND_INSTANE_ID = aws_instance.ondemand.*.id
   //ALL_INSTANCE_ID = concat(local.SPOT_INSTANCE_ID, local.ONDEMAND_INSTANE_ID)
   ALL_INSTANCE_ID = concat(local.SPOT_INSTANCE_ID)
   SPOT_PRIVATE_IP = aws_spot_instance_request.SPOT.*.private_ip
-  ONDEMAND_PRIVATE_IP = aws_instance.ondemand.*.private_ip
-  ALL_PRIVATE_IP = concat(local.SPOT_PRIVATE_IP,local.ONDEMAND_PRIVATE_IP)
+  //ONDEMAND_PRIVATE_IP = aws_instance.ondemand.*.private_ip
+  //ALL_PRIVATE_IP = concat(local.SPOT_PRIVATE_IP,local.ONDEMAND_PRIVATE_IP)
+  ALL_PRIVATE_IP = concat(local.SPOT_PRIVATE_IP,local)
 
 }
