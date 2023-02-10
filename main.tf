7712a99f076c2f6841d02bc87d000bb54e867aca
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-11 01:33:32 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "ondemand" {
-  count = var.instances["ONDEMAND"].instance_count
-  instance_type = var.instances["ONDEMAND"].instance_type
-  ami = data.aws_ami.ami.image_id
-  subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
-  vpc_security_group_ids = [aws_security_group.main.id]
-  iam_instance_profile = aws_iam_instance_profile.parameter-store-access.name
-
-}
+#resource "aws_instance" "ondemand" {
+#  count = var.instances["ONDEMAND"].instance_count
+#  instance_type = var.instances["ONDEMAND"].instance_type
+#  ami = data.aws_ami.ami.image_id
+#  subnet_id = data.terraform_remote_state.infra.outputs.app_subnets[count.index]
+#  vpc_security_group_ids = [aws_security_group.main.id]
+#  iam_instance_profile = aws_iam_instance_profile.parameter-store-access.name
+#
+#}
 resource "aws_spot_instance_request" "SPOT" {
   count = var.instances["SPOT"].instance_count
   instance_type = var.instances["SPOT"].instance_type
